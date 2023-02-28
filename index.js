ba212fa971c5704415dd8a35675f3bcd254afc22
Joern Bergmeier <joern@webcoffee.io>
2023-02-28 22:12:52 +0100

    kuehlschrank added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.command('temp', async (ctx, next) => {
   const start = new Date()
   next()
-  await getTemperature().then((data) => {
+  await getFermenterTemperature().then((data) => {
     const temp = "Fermenter Temp: " + data.toString() + "°C"
     console.log("data", data)
     ctx.telegram.sendMessage(ctx.message.chat.id, temp)
@@ -18,9 +18,42 @@ bot.command('temp', async (ctx, next) => {
   console.log('Response time: %sms', ms)
 })
 
-var getTemperature = function () {
+bot.command('tempKuehlschrank', async (ctx, next) => {
+    const start = new Date()
+    next()
+    await getFermenterTemperature().then((data) => {
+      const temp = "Kühlung Temp: " + data.toString() + "°C"
+      console.log("data", data)
+      ctx.telegram.sendMessage(ctx.message.chat.id, temp)
+      
+    })
+    const ms = new Date() - start
+    console.log('Response time: %sms', ms)
+  })
+
+var getFermenterTemperature = function () {
     return new Promise(function (resolve, reject) {
         axios.get('http://192.168.178.85:8000/sensor/Ey4P8xhZPRR5UpcnT3qVw9')
+        .then(function (response) {
+            // handle success
+            console.log(response.data.value);
+            resolve(response.data.value)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            reject(error)
+        })
+        .finally(function () {
+            // na
+        });
+
+    });
+  };
+
+  var getFridgeTemperature = function () {
+    return new Promise(function (resolve, reject) {
+        axios.get('http://192.168.178.85:8000/sensor/4QkExpiXTM6ECEEjbiZbti')
         .then(function (response) {
             // handle success
             console.log(response.data.value);
